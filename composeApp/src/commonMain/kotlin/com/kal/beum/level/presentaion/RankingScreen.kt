9fda908802f8f877615d9fc479512c78db564cea
junghee <junghee@nhn.com>
2025-07-15 00:44:36 +0900

    랭킹 구현

--- composeApp/src/commonMain/kotlin/com/kal/beum/level/presentaion/RankingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kal/beum/level/presentaion/RankingScreen.kt
@@ -3,21 +3,29 @@ package com.kal.beum.level.presentaion
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,6 +44,7 @@ import beumproject.composeapp.generated.resources.ic_devil_fire
 import beumproject.composeapp.generated.resources.sf_pro
 import coil3.compose.AsyncImage
 import com.kal.beum.core.presentation.BeumColors
+import com.kal.beum.core.presentation.BeumDimen
 import com.kal.beum.core.presentation.BeumTypo
 import com.kal.beum.home.presentation.components.ToggleButton
 import com.kal.beum.main.presentation.MainAction
@@ -43,11 +52,12 @@ import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RankingScreen(isDevil: Boolean, onAction: (MainAction) -> Unit) {
     val viewModel = koinViewModel<RankingViewModel>()
     val state by viewModel.state.collectAsStateWithLifecycle()
-
+    var showDetailInfoIndex by remember { mutableStateOf(-1) }
     LaunchedEffect(Unit) {
         viewModel.getRankerList(isDevil)
     }
@@ -75,89 +85,20 @@ fun RankingScreen(isDevil: Boolean, onAction: (MainAction) -> Unit) {
         LazyColumn {
             items(state.rankerUserList.size) { index ->
                 val ranker = state.rankerUserList[index]
-                Row(
-                    modifier = Modifier.fillMaxWidth().height(80.dp)
-                        .padding(vertical = 16.dp, horizontal = 20.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = ranker.rank.toString(), style = TextStyle(
-                            fontSize = 16.sp,
-                            lineHeight = BeumTypo.lineHeightBody3,
-                            fontFamily = FontFamily(Font(Res.font.sf_pro)),
-                            fontWeight = FontWeight(700),
-                            color = if (isDevil) if (ranker.rank > 3) BeumColors.White else BeumColors.DevilPrimary else if (ranker.rank > 3) BeumColors.Black else BeumColors.primaryPrimarySkyblue,
-                            textAlign = TextAlign.Center,
-                        )
-                    )
-                    Spacer(modifier = Modifier.width(12.dp))
-
-                    if (ranker.profileImageUrl.isNotEmpty()) {
-                        AsyncImage(
-                            model = ranker.profileImageUrl,
-                            contentDescription = "",
-                            modifier = Modifier.size(40.dp)
-                        )
-                    } else {
-                        Box(
-                            modifier = Modifier.border(
-                                width = 0.625.dp,
-                                color = BeumColors.baseAlphaWhiteLightWhite500A,
-                                shape = RoundedCornerShape(size = 20.dp)
-                            ).width(24.dp).height(24.dp).background(
-                                color = if (isDevil) BeumColors.DevilPrimary else Color(0xFF45CAF7), shape = RoundedCornerShape(size = 20.dp)
-                            )
-                        ) {
-                            Image(
-                                painter = painterResource(if (isDevil) Res.drawable.devil else Res.drawable.angel),
-                                contentDescription = "",
-                                modifier = Modifier.size(40.dp)
-                            )
-                        }
-                    }
-                    Spacer(modifier = Modifier.width(12.dp))
-                    Text(
-                        text = ranker.nickname, modifier = Modifier.weight(1f), style = TextStyle(
-                            fontSize = 16.sp,
-                            lineHeight = BeumTypo.lineHeightBody3,
-                            fontFamily = FontFamily(Font(Res.font.sf_pro)),
-                            fontWeight = FontWeight(700),
-                            color = if (isDevil) BeumColors.White else BeumColors.baseAlphaBlackDarkBlack700A,
-                        )
-                    )
-
-                    Text(
-                        text = ranker.score.toString(), style = TextStyle(
-                            fontSize = BeumTypo.TypoScaleText200,
-                            lineHeight = BeumTypo.lineHeightCaption1,
-                            fontFamily = FontFamily(Font(Res.font.sf_pro)),
-                            fontWeight = FontWeight(400),
-                            color = if (isDevil) BeumColors.baseAlphaWhiteLightWhite500A else BeumColors.baseAlphaBlackDarkBlack500A,
-                            textAlign = TextAlign.Right,
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.width(4.dp))
-
-                    Box(
-                        modifier = Modifier.width(16.dp).height(16.dp).background(
-                            color = BeumColors.baseCoolGrayLightGray700,
-                            shape = RoundedCornerShape(size = 66.66668.dp)
-                        ).padding(
-                            start = 2.66667.dp,
-                            top = 2.66667.dp,
-                            end = 2.66667.dp,
-                            bottom = 2.66667.dp
-                        ), contentAlignment = Alignment.Center
-                    ) {
-                        Image(
-                            painter = if (isDevil) painterResource(Res.drawable.ic_devil_fire) else painterResource(Res.drawable.heart), contentDescription = ""
-                        )
-                    }
+                RankDetailInfo(isDevil, ranker) {
+                    showDetailInfoIndex = index
                 }
             }
-
+        }
+        if (showDetailInfoIndex >= 0) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showDetailInfoIndex = -1
+                }, // 닫힐 때 None으로 초기화
+                containerColor = if (isDevil) Color(0xCC000000) else Color(0xCCFFFFFF	),
+            ) {
+                RankDetailInfo(isDevil, state.rankerUserList[showDetailInfoIndex]) {}
+            }
         }
     }
-
 }
